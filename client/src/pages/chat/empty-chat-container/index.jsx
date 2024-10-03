f8cd66853c0b3802f28fcd7c61b418950fc3c6e4
mrsidyot77 <mrsidyot@gmail.com>
2024-10-03 14:44:23 +0530

    responsive emptyChatContainer

--- client/src/pages/chat/empty-chat-container/index.jsx
+++ client/src/pages/chat/empty-chat-container/index.jsx
@@ -4,18 +4,19 @@ import { animationDefaultOptions } from '@/lib/utils'
 
 function EmptyChatContainer() {
   return (
-    <div className='flex-1 md:bg-[#1c1d25] md:flex flex-col justify-center items-center hiiden duration-1000 transition-all'>
-      <Lottie 
-        isCliclToPauseDisabled={true}
-        height={200}
-        width={200}
-        options={animationDefaultOptions}
-      />
+    <div className='flex-1 bg-[#1c1d25] md:bg-[#1c1d25] flex flex-col justify-center items-center hidden md:flex transition-all duration-1000'>
+      <div className='w-[70%] max-w-[200px]'>
+        <Lottie
+          isClickToPauseDisabled={true}
+          options={animationDefaultOptions}
+          style={{ width: '100%', height: 'auto' }}
+        />
+      </div>
       <div className='text-opacity-80 text-white flex flex-col items-center mt-10 gap-5 lg:text-4xl text-3xl transition-all duration-300 text-center'>
         <h3 className='poppins-medium'>
-            Hi<span className='text-purple-500' >! </span>Welcome to
-            <span className='text-purple-500'> SpiceChat </span>App
-            <span className='text-purple-500'>...</span>
+          Hi<span className='text-purple-500'>! </span>Welcome to
+          <span className='text-purple-500'> SpiceChat </span>App
+          <span className='text-purple-500'>...</span>
         </h3>
       </div>
     </div>
